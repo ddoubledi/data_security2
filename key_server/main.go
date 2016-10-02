e1418aeee884408af5d41bb2cecf10210679b8c4
pavel-zhuravlyov <pavlozhuravlyov@gmail.com>
2016-10-02 21:45:36 +0300

    change something

--- key_server/main.go
+++ key_server/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// "strings"
+	"strings"
 	"regexp"
 	"github.com/monnand/dhkx"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"encoding/base64"
+	"io"
 )
 
 func main() {
@@ -30,25 +36,83 @@ func checkError(err error) {
 
 func handleClient(conn net.Conn) {
 	hello, _ := regexp.Compile("^client hello")
-	done, _ := regexp.Compile("^client done;")
+	done, _ := regexp.Compile("^client done$")
+
 	buf := make([]byte, 128)
 	defer conn.Close()
+
+	// TODO refactor this
+	group, _ := dhkx.GetGroup(0)
+	privateKey, _ := group.GeneratePrivateKey(nil)
+	publicKey := privateKey.Bytes()
+
 	for {
 		readLen, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println(err)
 		}
 		if readLen != 0 {
-			if hello.MatchString(string(buf)) {
-				group, _ := dhkx.GetGroup(0)
-				privateKey, _ := group.GeneratePrivateKey(nil)
-				publicKey := privateKey.Bytes()
-				responce := append([]byte("server hello;"), publicKey...)
+			res := strings.Split(string(buf), ";")
+
+			if hello.MatchString(string(res[0])) {
+				responce := append([]byte(";"), publicKey...)
 				conn.Write([]byte(responce))
-			} else if done.MatchString(string(buf)) {
+			}
+
+			k, _ := group.ComputeKey(dhkx.NewPublicKey([]byte(res[1])), privateKey)
+			sessionKey := generateAESKey(k.Bytes())
+			message, _ := decrypt(sessionKey, []byte(res[0]))
+
+			if done.MatchString(string(message)) {
 				conn.Write([]byte("server done"))
 			}
 		}
 		buf = make([]byte, 128)
 	}
+}
+
+func encrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	b := base64.StdEncoding.EncodeToString(text)
+	ciphertext := make([]byte, aes.BlockSize + len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
+	return ciphertext, nil
+}
+
+func decrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(text) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	data, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func generateAESKey(largeKey []byte) []byte {
+	key := make([]byte, 32)
+
+	key = largeKey[78:110]
+	for i := range key {
+		key[i] += largeKey[i + 10]
+	}
+
+	return key
 }
